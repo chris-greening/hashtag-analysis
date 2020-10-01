aafccc327334a0645c8460287d4e8faffea9d3db
chris-greening <chrisgreening3@gmail.com>
2020-10-01 18:20:44 -0400

    Added progress bar for scrape

--- scraper.py
+++ scraper.py
@@ -7,10 +7,12 @@ import datetime
 import time
 import csv
 
+import tqdm
 import numpy as np
 from instascrape import Hashtag
 
 def _load_data_into_files(data, fp):
+    """Load data into .csv"""
     with open(fp, 'a', newline='') as out:
         w = csv.DictWriter(out, fieldnames=['hashtag', 'posts', 'datetime'])
         if os.path.getsize(fp) == 0:
@@ -27,15 +29,17 @@ def _load_hashtags_from_txt(fpath: str) -> list :
 def _load_hashtag_data(tags: list):
     """Scrape hashtag data for the given hashtags"""
     hashtag_objs = []
-    for tag in tags:
-        time.sleep(abs(np.random.normal(3, 2)))
-        hashtag = Hashtag.from_hashtag(tag)
-        hashtag.static_load()
-        hashtag_objs.append({
-            "hashtag": tag,
-            "posts": hashtag.amount_of_posts,
-            "datetime": datetime.datetime.now()
-        })
+    with tqdm.tqdm(total=len(tags), position = 0) as progress:
+        for tag in tags:
+            time.sleep(abs(np.random.normal(3, 2)))
+            hashtag = Hashtag.from_hashtag(tag)
+            hashtag.static_load()
+            hashtag_objs.append({
+                "hashtag": tag,
+                "posts": hashtag.amount_of_posts,
+                "datetime": datetime.datetime.now()
+            })
+            progress.update(1)
     return hashtag_objs
 
 def main(hashtag_fpath, hashtag_csv):
